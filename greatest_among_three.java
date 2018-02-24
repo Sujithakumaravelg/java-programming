807645a3e56189e1a812ad718c1e648cffe17c8f
Sujithakumaravelg <36624092+Sujithakumaravelg@users.noreply.github.com>
2018-02-24 20:59:34 +0530

    Update greatest_among_three.java

--- greatest_among_three.java
+++ greatest_among_three.java
@@ -7,11 +7,11 @@ public class Alpha{
 		        int b = s.nextInt();
 		        int c = s.nextInt();
             if(a>b && a>c)
-		          System.out.println("First number is greater");
+		          System.out.println(a);
 		        else if(b>a && b>c)
-		          System.out.println("Second number is greater");
+		          System.out.println(b);
 		        else
-		          System.out.println("Third number is greater");
+		          System.out.println(c);
         }
         catch(Exception e){
             System.out.println("enter valid number");
