f356d77dcd4b2af46ca9c347490eecd0cf7e8d3b
Sujithakumaravelg <36624092+Sujithakumaravelg@users.noreply.github.com>
2018-02-25 09:42:25 +0530

    Update MaxElementInArray.java

--- MaxElementInArray.java
+++ MaxElementInArray.java
@@ -3,18 +3,23 @@ public class MaxElementInArray{
    public static void main(String arg[])throws Exception {
       try{
          Scanner s = new Scanner(System.in);
-         int n = s.nextInt(),max,a[] = new int[n];
+         int n = s.nextInt();
+         if(n>0 && n<=100000){
+            int max,a[] = new int[n];
          for(int i = 0; i < n; i++)
             a[i] = s.nextInt();
          max = a[0];
          for(int i = 0; i < n; i++){
             if(max < a[i])
                 max = a[i];
-            }
-        System.out.println(max);
-      }  
-        catch(Exception e){
+         }
+         System.out.println(max);
+         }
+         else
             System.out.println("enter valid value");
-        }
+      }  
+      catch(Exception e){
+            System.out.println("enter valid value");
+      }
     }
 }
