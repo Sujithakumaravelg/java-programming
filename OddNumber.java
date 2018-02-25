5584de740ad446480a1743c7491e0955c0092ab7
Sujithakumaravelg <36624092+Sujithakumaravelg@users.noreply.github.com>
2018-02-25 06:40:53 +0530

    Update OddNumber.java

--- OddNumber.java
+++ OddNumber.java
@@ -5,7 +5,7 @@ public class OddNumber{
             Scanner s = new Scanner(System.in);
             int num1=s.nextInt();
 	          int num2=s.nextInt();
-	          for(int i=num1;i<=num2;i++){
+	          for(int i=(num1+1);i<num2;i++){
 	              if(i%2!=0){
 	                System.out.println(i);
 	              }
