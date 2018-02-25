59a6fe4dc8f38ce21564b628944eafb2a298a7c3
Sujithakumaravelg <36624092+Sujithakumaravelg@users.noreply.github.com>
2018-02-25 15:21:35 +0530

    Update WordsInALine.java

--- WordsInALine.java
+++ WordsInALine.java
@@ -4,13 +4,18 @@ public class WordsInALine{
         try{
             Scanner sc=new Scanner(System.in); 
             String s=sc.nextLine();
-            int count = 1;
-            for (int i = 0; i < s.length() - 1; i++){
+            int len=s.length();
+            if(len<=1000){
+                int count = 1;
+                for (int i = 0; i < (len - 1); i++){
 			        if ((s.charAt(i) == ' ') && (s.charAt(i + 1) != ' '))
 			          count++;
-            }
+                }
  		        System.out.println("Number of words in a string = " + count);
-	      }
+	        }
+	        else
+	            System.out.println("enter valid value");
+        }
         catch(Exception e){
             System.out.println("enter valid value");
         }
