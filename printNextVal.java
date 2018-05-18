4862f39a2a4b5ed3b5338b09b38c0fe78aed8fed
Sujithakumaravelg <36624092+Sujithakumaravelg@users.noreply.github.com>
2018-05-17 20:21:43 -0700

    Create printNextVal.java

---
+++ printNextVal.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class printNextVal{
+   public static void main(String arg[])throws Exception {
+      try{
+          int i=0;
+          Scanner s = new Scanner(System.in);
+          int n=s.nextInt();
+          System.out.println(n+1);
+        }
+     catch(Exception e){
+            System.out.println("enter valid value");
+      }
+    }
+}
