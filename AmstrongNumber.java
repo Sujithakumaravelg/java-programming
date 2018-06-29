0eab1c110d42b19654112c3084fc78e4917a024b
Sujithakumaravelg <36624092+Sujithakumaravelg@users.noreply.github.com>
2018-06-29 09:39:20 +0530

    Update AmstrongNumber.java

--- AmstrongNumber.java
+++ AmstrongNumber.java
@@ -1,22 +1,30 @@
 import java.util.Scanner;
 public class AmstrongNumber{
-    public static void main(String arg[])throws Exception {
-        try{
-            Scanner s = new Scanner(System.in);
-            int num=s.nextInt(),m,sum=0,p,i;
-            p=num;
-            for(i=0;num>0;i++){
-                m=num%10;
-                sum=sum+m*m*m;
-                num=num/10;
-            }
-            if(sum==p)
-                System.out.println("yes");
-            else
-                System.out.println("no");
-        } 
-        catch(Exception e){
-            System.out.println("enter valid value");
-        }
-    }
+	public static void main(String a[]){
+		try{
+			Scanner s =new Scanner(System.in);
+			int in=s.nextInt();
+			int ans=AmstrongNumber.Amstrong(in);
+			System.out.println(ans);
+			}
+		
+		catch(Exception e){
+			System.out.println(e);
+		}
 }
+	static int Amstrong(int input1){
+		int num=input1,sum=0,rem,rval;
+		while(num!=0){
+			rem=num%10;
+			sum=sum+(rem*rem*rem);
+			num=num/10;
+		}
+		if(sum==input1)
+			rval=2;
+		else
+			rval=1;
+		return rval;
+	}
+}
+//return 2 when the given input is AmstrongNumber 
+//return 1 when the given input is not a AmstrongNumber 
