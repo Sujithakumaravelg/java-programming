4f242a8c6593e8d8e2db1530057d1db2495f118e
Sujithakumaravelg <36624092+Sujithakumaravelg@users.noreply.github.com>
2018-06-29 10:15:20 +0530

    Update ArithmeticProgression.java

--- ArithmeticProgression.java
+++ ArithmeticProgression.java
@@ -1,19 +1,26 @@
 import java.util.Scanner;
 public class ArithmeticProgression{
-   public static void main(String arg[])throws Exception {
-      try{
-         Scanner s=new Scanner(System.in);   
-         
-         int A=s.nextInt(),n=s.nextInt(),d=s.nextInt(),i; 
-         int a[]=new int[(n+1)];
-         a[1]=A;
-         for(i=2;i<=n;i++)
-            a[i]=a[1]+(i-1)*d;
-         for(i=1;i<=n;i++)
-            System.out.print(a[i]+"\t");
-      }  
-        catch(Exception e){
-            System.out.println("enter valid value");
-        }
-    }
+	public static void main(String arg[])throws Exception {
+		try{
+			Scanner s =new Scanner(System.in);
+			int in1=s.nextInt(),d=s.nextInt(),n=s.nextInt();
+			int[] ans=ArithmeticProgression.A_P(in1,d,n);
+			for(int i=0;i<n;i++){
+				System.out.print(ans[i]+"\t");
+			}
+		}
+		catch(Exception e){
+			System.out.println(e);
+		}
 }
+	static int[] A_P(int input1,int input2,int input3){
+		int A[]=new int[input3];
+		A[0]=input1;
+		for(int i=1;i<input1;i++){
+			A[i]=A[0]+(i)*(input2);
+		}
+		return A;
+	}
+}
+//Find the arithmetic progression given in1="starting value of series",d="arithmetic difference",n="length of the series"
+//eg in1=5 d=-3 n=5      A[]={5,2,-1,-4,-7}
