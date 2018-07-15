75e4a86c0b09508b9eee894e19c9c96f398465d4
Sujithakumaravelg <36624092+Sujithakumaravelg@users.noreply.github.com>
2018-07-14 17:09:10 -0700

    Add files via upload

---
+++ RevStrByEliminatingVowels.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class RevStrByEliminatingVowels extends Exception{
+	public static void main(String args[]){
+		try{
+			Scanner s=new Scanner(System.in);
+			int num=s.nextInt();
+			String str=s.next();
+			char ch[]=str.toCharArray();
+			if(ch.length!=num){
+				throw new Exception();
+			}
+			int j=0;
+			char ch1[]=new char[num];
+			for(int i=0;i<num;i++){
+				if(ch[i]!='a'&&ch[i]!='e'&&ch[i]!='i'&&ch[i]!='o'&&ch[i]!='u'&&ch[i]!='A'&&ch[i]!='E'&&ch[i]!='I'&&ch[i]!='O'&&ch[i]!='U'){
+					ch1[j]=ch[i];
+					j++;
+				}
+			}
+		StringBuilder str1=new StringBuilder();
+			for(int i=ch1.length-1;i>-1;i--){
+				str1.append(ch1[i]);
+			}
+			System.out.println(str1);
+			
+		}
+		catch(Exception e){
+			System.out.println("Enter a valid value");
+		}
+	}
+}
