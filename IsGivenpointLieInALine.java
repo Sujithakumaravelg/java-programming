0e4b70e7b37f1eaa386e84c6c33fd79f6dff7543
Sujithakumaravelg <36624092+Sujithakumaravelg@users.noreply.github.com>
2018-07-14 19:24:19 -0700

    Add files via upload

---
+++ IsGivenpointLieInALine.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class IsGivenpointLieInALine{
+	public static void main(String args[]){
+	try{
+		Scanner s=new Scanner(System.in);
+		int arr[]=new int[6];
+		String str="No";
+		for(int i=0;i<6;i++){
+			arr[i]=s.nextInt();
+		}
+		if(arr[0]==arr[2] && arr[0]==arr[4])
+			str="Yes";
+		else if(arr[1]==arr[3] && arr[1]==arr[5])
+			str="Yes";
+		System.out.println(str);
+		}
+		catch(Exception e){
+			System.out.println("Enter Valid Value");
+		}
+	}
+}
+	
